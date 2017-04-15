8d108c6b9af934de9a1d70b2bd391581ca63c7cd
mihai-burduselu <michelcatalin@gmail.com>
2017-04-15 21:20:24 +0300

    Add etouches API testing server

---
+++ etouches API/etouches API/Controllers/SurveyController.cs
@@ -0,0 +1,29 @@
+﻿using etouches_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace etouches_API.Controllers
+{
+    public class SurveyController : ApiController
+    {
+        public static int count = 0;
+        private static Survey Survey = null;
+
+        [HttpGet]
+        public Survey GetSurvey()
+        {
+            return Survey;
+        }
+
+        [HttpPost]
+        public void PostSurvey(Survey survey)
+        {
+            Survey = survey;
+            count++;
+        }
+    }
+}
